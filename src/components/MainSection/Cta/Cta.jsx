4a20ff7ca861b2e3902311c5efc3055f3df733c3
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-07-29 11:27:28 +0200

    style CTA section

--- src/components/MainSection/Cta/Cta.jsx
+++ src/components/MainSection/Cta/Cta.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 function Cta() {
   return (
-    <section className="cta | padding-block-700 bg-accent-400 text-neutral-100">
+    <section className="cta | padding-block-900 bg-accent-400 text-neutral-100">
       <div className="wrapper">
-        <div className="even-columns">
+        <div className="even-columns vertical-align-center">
           <div>
-            <p className="fs-secondary-heading fw-bold">
+            <p className="fs-primary-heading fw-bold">
               Simplify how your team works today.
             </p>
           </div>
-          <div>
-            <button className="button button--white">Get Started</button>
+          <div className="justify-self-end-md">
+            <button className="button | button--white">Get Started</button>
           </div>
         </div>
       </div>
